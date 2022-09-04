5ed38278acec70fccb6f1c89e108e4c52e606df0
Ashraf Hafiz <Ashraf.Hafiz@yahoo.com>
2022-09-04 21:17:37 +0200

    2nd commit

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
 import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
+import { sliderItems } from "../slideData";
 
 const Container = styled.div`
   width: 100%;
@@ -9,6 +10,7 @@ const Container = styled.div`
   display: flex;
   background-color: antiquewhite;
   position: relative;
+  overflow: hidden;
 `;
 
 const ArrowContainer = styled.div`
@@ -27,15 +29,84 @@ const ArrowContainer = styled.div`
   right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
   opacity: 0.5;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  background-color: ${(props) => props.bg};
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+
+const Description = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <Container>
-      <ArrowContainer direction="left">
+      <ArrowContainer direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftOutlinedIcon />
       </ArrowContainer>
-      <ArrowContainer direction="right">
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item, index) => (
+          <Slide bg={item.bg} key={index}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Description>{item.description}</Description>
+              <Button>SHOW NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <ArrowContainer direction="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlinedIcon />
       </ArrowContainer>
     </Container>
---
+++ src/slideData.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://cdn.pixabay.com/photo/2016/11/21/16/55/high-heels-1846436_960_720.jpg",
+    title: "SUMMER SALE",
+    description: "DON'T COMPROMISE ON STYLE. GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "#f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://cdn.pixabay.com/photo/2016/10/16/23/32/fashion-show-1746581_960_720.jpg",
+    title: "WINTER SALE",
+    description: "DON'T COMPROMISE ON STYLE. GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "#fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://cdn.pixabay.com/photo/2016/10/16/23/32/fashion-show-1746579_960_720.jpg",
+    title: "POPULAR SALE",
+    description: "DON'T COMPROMISE ON STYLE. GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "#fbf0f4",
+  },
+];
